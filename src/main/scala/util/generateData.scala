b88d9e06e09ea9f29bf1b7f112954bf92fedbbaa
xiexiaofeng <jftrace@outlook.com>
2022-09-03 19:02:21 +0900

    mockData fix

--- src/main/scala/util/generateData.scala
+++ src/main/scala/util/generateData.scala
@@ -21,16 +21,16 @@ class generateData {
    */
   def UserLog(): Seq[(String,String,String,String,String,String,String)] = {
     val log = new ListBuffer[(String,String,String,String,String,String,String)]
-    for(i<- 0 until 100000){
+    for(i<- 0 until 1000){
       //UserID example: M717
-      val userID = "M" + random.nextInt(100000)
+      val userID = "M" + random.nextInt(10000)
       for(j<-0 until 10){
         val sessionID = UUID.randomUUID().toString.replace("-","")
         for (k<-0 to random.nextInt(30)){
           val now = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
             .format(LocalDateTime.now().plusSeconds(random.nextInt(3600)))
           val pageID = pageList(random.nextInt(7)) + ""
-          val jobID = "M" + random.nextInt(10000)
+          val jobID = "M" + random.nextInt(1000)
           val jobInfo = UUID.randomUUID().toString.split("-")(0)
           val applyAction = if(pageID == "7") "1" else "0"
           val row = (now,userID,sessionID,pageID,jobID,jobInfo,applyAction)
--- src/main/scala/util/mockData.scala
+++ src/main/scala/util/mockData.scala
@@ -12,13 +12,14 @@ object mockData {
       .getOrCreate()
     val mock = new generateData()
 
-    for (i<-1 to 5){
+    for (i<-1 to 100){
       val userOneLog = mock.UserLog()
       val rdd = sc.parallelize(userOneLog)
       val df = sparkSession.createDataFrame(rdd = rdd)
         .toDF("AccessTime","UserID","SessionID","PageID","JobID","JobInfo","ApplyAction")
 
       df.write.option("compression", "gzip").json(s"gs://user-log-data-uscentral1/user_log_$i")
+      println(i + " time is over")
     }
 
     sparkSession.stop()
